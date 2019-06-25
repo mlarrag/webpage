c5455787bf9d6ea2ac38e59eb22793e9231be2fd
Matias Larraguibel <mlarra@gmail.com>
2019-06-25 19:09:54 -0400

    inicios de tarjetas home

--- abstract-homepage/src/components/homeCard/homecard.js
+++ abstract-homepage/src/components/homeCard/homecard.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import Content1 from '../assets/images/Content1/content1.png';
-import { Menu, Dropdown, Grid, Segment, Header, Image, Icon, Responsive, Sidebar, Container, Button, DropdownMenu, DropdownItem, GridColumn } from 'semantic-ui-react';
-
-import './homecontent1.css'
+import VolvoCard from '../assets/images/Volvo/card.png';
+import VolvoCardLogo from '../assets/images/Volvo/cardlogo.png';
+import { Menu, Dropdown, Grid, Segment, Header, Image, Icon, Responsive, Sidebar, Container, Button, DropdownMenu, DropdownItem, GridColumn, Card } from 'semantic-ui-react';
 
 
 class HomeCard extends Component {
@@ -12,8 +11,17 @@ class HomeCard extends Component {
   
       return (
         
-           HomeCard  
-            
+        <Card>
+         <Image src={VolvoCard} circular size="medium" bordered centered/>
+         <Image src={VolvoCardLogo} avatar centered/>
+             <Card.Content>
+        <Card.Description>
+        "Abstract nos brinda un desarrollo oportuno y completo, abarcando  
+        </Card.Description>
+        </Card.Content>
+
+        </Card>
+                
       )
     }
   }
--- abstract-homepage/src/components/homecontent1/homecontent1.js
+++ abstract-homepage/src/components/homecontent1/homecontent1.js
@@ -3,6 +3,7 @@ import Content1 from '../assets/images/Content1/content1.png';
 import { Menu, Dropdown, Grid, Segment, Header, Image, Icon, Responsive, Sidebar, Container, Button, DropdownMenu, DropdownItem, GridColumn } from 'semantic-ui-react';
 
 import './homecontent1.css'
+import HomeCard from '../homeCard/homecard';
 
 
 class HomeContent1 extends Component {
@@ -26,6 +27,8 @@ class HomeContent1 extends Component {
             <Header as='h2' className="header3">Somos un equipo de expertos apasionados por crear soluciones innovadoras 
 </Header>
             <Header as='h3' className="header4">Entregamos productos de valor a nuestros clientes</Header>
+
+            <HomeCard/>
                 </div>
 
                     
